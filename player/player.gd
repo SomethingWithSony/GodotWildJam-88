06c23a1ecdc484adf63d5fcb2e8a2497e9c3d770
Sony <143217009+SomethingWithSony@users.noreply.github.com>
2025-12-14 15:55:01 +0000

    Add: Ability to kill enemies

--- player/player.gd
+++ player/player.gd
@@ -5,8 +5,8 @@ extends CharacterBody3D
 # Movement
 @export var max_speed : float = 100
 @export var acceleration : float = 10
-@export var dash_strength: float = 20
-
+@export var dash_strength: float = 30
+@export var bounce_strength: float = 15 
 var movement_direction: Vector2 = Vector2.ZERO
 
 # Jump
@@ -16,10 +16,16 @@ const GRAVITY: float = -9.8
 # Combat
 var current_damage: float = 1
 @onready var camera: Camera3D = $Camera3D
-@onready var pivot = $Pivot
+@onready var pivot: Node3D = $Pivot
+@onready var attack_area: Area3D = $AttackArea
+@onready var dash_attack_timer: Timer = $DashAttackTimer
+var in_slow_motion: bool = false
 
 func _ready():
 	pivot.hide()
+	attack_area.monitoring = false
+	# Ensure attack area detects enemies even if physics body ignores them
+	attack_area.collision_mask = 2
 
 func _process(_delta):
 	if Input.is_action_just_pressed("slow_down_time"):
@@ -65,17 +71,56 @@ func _physics_process(delta):
 		var angle = atan2(look_target.x - global_position.x, look_target.z - global_position.z)
 		pivot.rotation.y = lerp_angle(pivot.rotation.y, angle , 1)
 		
-		if Input.is_action_just_pressed("dash"):
+		if Input.is_action_just_pressed("dash") and in_slow_motion:
 			var dash_direction: Vector3 = (look_target - global_position).normalized()
-			velocity = dash_direction * dash_strength
+			start_dashing(dash_direction)
 			
-
-	#velocity = velocity.move_toward(new_velocity, acceleration)
 	move_and_slide()
 
+func start_dashing(target_pos: Vector3):
+	velocity = target_pos * dash_strength
+	
+	# Turn on attack collision detection
+	attack_area.monitoring = true
+	
+	# Turn of collision with enemies - Layer 2
+	set_collision_mask_value(2, false)
+	
+	dash_attack_timer.start()
+
+func end_dash():
+	# Turn off  attack collision detection
+	attack_area.monitoring = false
+	
+	# Turn on collision with enemies - Layer 2
+	set_collision_mask_value(2, false)
+	
 func slow_down_time():
 	Engine.time_scale = 0.2
+	in_slow_motion = true
 	
 func reset_time():
 	Engine.time_scale = 1
-	
+	in_slow_motion = false
+
+func _on_attack_area_body_entered(body):
+	if body is Enemy:
+		var is_dead = body.take_damage(current_damage)
+		
+		if is_dead:
+			# Do nothing
+			# The enemy dies and the player phases trough it
+			pass
+		else:
+			# Bounce off the enemy
+			var bounce_direction = (global_position - body.global_position).normalized()
+			
+			# Bounce off immediately
+			velocity = bounce_direction * bounce_strength
+			
+			# Ensure we dont hit an enemy twice
+			end_dash()
+			dash_attack_timer.stop()
+			
+func _on_dash_attack_timer_timeout():
+	end_dash()
